59bbab68f0f4b643a7da8ddab778a8d0958bb86f
Jeff Brown <jeff@jeffandbetsy.net>
2012-01-10 14:50:08 -0600

    add place holder test

---
+++ test/unit/PlaceholderTests.groovy
@@ -0,0 +1,5 @@
+// this is just a placeholder until code is written. 
+// we need a test in place for the CI build to pass
+class PlaceholderTests extends GroovyTestCase {
+    void testNothing(){}
+}
