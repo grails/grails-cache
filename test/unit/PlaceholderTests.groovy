475fa5aa62aa39a41f158338e591a6fd12bef979
Jeff Brown <jeff@jeffandbetsy.net>
2012-03-05 09:04:28 -0600

    remove placeholder test

--- test/unit/PlaceholderTests.groovy
+++
@@ -1,5 +0,0 @@
-// this is just a placeholder until code is written. 
-// we need a test in place for the CI build to pass
-class PlaceholderTests extends GroovyTestCase {
-    void testNothing(){}
-}
