0a7477aa6fbf15da8109be23376706d8a5e5f2c2
Burt Beckwith <beckwithb@vmware.com>
2012-04-16 16:51:47 -0400

    upgrade to v2.0.0 of release plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,5 +1,6 @@
 grails.project.work.dir = 'target'
 grails.project.docs.output.dir = 'docs/manual' // for gh-pages branch
+grails.project.source.level = 1.6
 
 grails.project.dependency.resolution = {
 
@@ -15,7 +16,7 @@ grails.project.dependency.resolution = {
 			export = false
 		}
 		if (Environment.current != Environment.TEST) {
-			build(':release:1.0.1', ':svn:1.0.2') {
+			build(':release:2.0.0', ':rest-client-builder:1.0.2') {
 				export = false
 			}
 		}
