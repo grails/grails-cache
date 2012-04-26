a406168d7bdeb9851358d70e8fea6d3ea6721a96
Jeff Brown <jeff@jeffandbetsy.net>
2012-04-26 07:38:41 -0500

    added some javadoc

--- grails-app/taglib/org/grails/plugin/cache/CacheTagLib.groovy
+++ grails-app/taglib/org/grails/plugin/cache/CacheTagLib.groovy
@@ -6,6 +6,12 @@ class CacheTagLib {
 
     def grailsCacheManager
 
+    /**
+     * Renders a block of markup and caches the result so the next time the same block
+     * is rendered, it does not need to be evaluated again.
+     * 
+     * @attr key An optional cache key allowing the same block to be cached with different content
+     */
     def block = { attrs, body ->
         def cache = grailsCacheManager.getCache('grailsBlocksCache')
         def bodyClosure = body.@bodyClosure
@@ -24,6 +30,13 @@ class CacheTagLib {
         out << content
     }
 
+    /**
+     * Renders a GSP template and caches the result so the next time the same template 
+     * is rendered, it does not need to be evaluated again.
+     * 
+     * @attr template REQUIRED The name of the template to apply
+     * @attr key An optional cache key allowing the same template to be cached with different content
+     */
     def render =  { attrs ->
         // TODO using attrs.template is not adequate here, we need the full path to the template
         def key = attrs.template
