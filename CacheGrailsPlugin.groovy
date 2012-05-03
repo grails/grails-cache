2f055d32c1d3d8dd5f71c97ee00d24aa46e554a9
Burt Beckwith <beckwithb@vmware.com>
2012-05-03 00:46:29 -0400

    1.0.0.M1 release

--- CacheGrailsPlugin.groovy
+++ CacheGrailsPlugin.groovy
@@ -33,7 +33,7 @@ class CacheGrailsPlugin {
 
 	private final Logger log = LoggerFactory.getLogger('grails.plugin.cache.CacheGrailsPlugin')
 
-	def version = '0.5.BUILD-SNAPSHOT'
+	def version = '1.0.0.M1'
 	def grailsVersion = '2.0 > *'
 	def observe = ['controllers']
 	def loadAfter = ['controllers']
