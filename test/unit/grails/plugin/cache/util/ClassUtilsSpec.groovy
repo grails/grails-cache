05a3c2b7937f989e209ad57ac572d0762c0bf38d
Burt Beckwith <beckwithb@vmware.com>
2012-05-03 02:36:01 -0400

    restoring Spock plugin and test

--- test/unit/grails/plugin/cache/util/ClassUtilsSpec.groovy
+++ test/unit/grails/plugin/cache/util/ClassUtilsSpec.groovy
@@ -1,9 +1,28 @@
 package grails.plugin.cache.util
 
+import spock.lang.Specification
+import spock.lang.Unroll
+
 /**
  * @author Jeff Brown
  */
-class ClassUtilsSpec {
+class ClassUtilsSpec extends Specification {
+
+    @Unroll("#fieldOrPropertyName value should have been #expectedValue")
+    def 'Test retrieving field or property value'() {
+        given:
+        def obj = new SomeGroovyClass()
+        
+        expect:
+        expectedValue == ClassUtils.getPropertyOrFieldValue(obj, fieldOrPropertyName)
+        
+        where:
+        fieldOrPropertyName        | expectedValue
+        'someProperty'             | 1
+        'publicField'              | 2
+        'privateField'             | 3
+        'propertyWithPrivateField' | 100
+    }
 }
 
 class SomeGroovyClass {
