af580e38422f16d130dfd1032af2fbfe4b267edc
Burt Beckwith <beckwithb@vmware.com>
2012-05-20 16:12:46 -0400

    fixed packages

--- grails-app/services/grails/plugin/cache/GrailsCacheAdminService.groovy
+++ grails-app/services/grails/plugin/cache/GrailsCacheAdminService.groovy
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.grails.plugin.cache
+package grails.plugin.cache
 
 import grails.plugin.cache.CacheEvict
 
