29b948915d9ca992b213f5ef6aba03e5f3f1efe6
Burt Beckwith <beckwithb@vmware.com>
2012-05-20 01:27:58 -0400

    adding more information to the cache key to ensure that different requests have unique keys

--- src/java/grails/plugin/cache/web/filter/DefaultWebKeyGenerator.java
+++ src/java/grails/plugin/cache/web/filter/DefaultWebKeyGenerator.java
@@ -16,6 +16,9 @@ package grails.plugin.cache.web.filter;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.codehaus.groovy.grails.web.util.WebUtils;
+import org.springframework.util.StringUtils;
+
 /**
  * Default implementation.
  *
@@ -23,11 +26,38 @@ import javax.servlet.http.HttpServletRequest;
  */
 public class DefaultWebKeyGenerator implements WebKeyGenerator {
 
+	protected boolean supportAjax = false;
+
+	public static final String X_REQUESTED_WITH = "X-Requested-With";
+
 	public String generate(HttpServletRequest request) {
-		String key = request.getMethod().toUpperCase() + ':' + request.getRequestURI();
-		if (request.getQueryString() != null) {
-			key += '?' + request.getQueryString();
+
+		String uri = WebUtils.getForwardURI(request);
+
+		StringBuilder key = new StringBuilder();
+		key.append(request.getMethod().toUpperCase());
+
+		String format = WebUtils.getFormatFromURI(uri);
+		if (StringUtils.hasLength(format) && !"all".equals(format)) {
+			key.append(":format:").append(format);
 		}
-		return key;
+
+		if (supportAjax) {
+			String requestedWith = request.getHeader(X_REQUESTED_WITH);
+			if (StringUtils.hasLength(requestedWith)) {
+				key.append(':').append(X_REQUESTED_WITH).append(':').append(requestedWith);
+			}
+		}
+
+		key.append(':').append(uri);
+		if (StringUtils.hasLength(request.getQueryString())) {
+			key.append('?').append(request.getQueryString());
+		}
+
+		return key.toString();
+	}
+
+	public void setSupportAjax(boolean support) {
+		supportAjax = support;
 	}
 }
