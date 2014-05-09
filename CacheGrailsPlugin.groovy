44523e5fd7cc800efb7525ce56d556ac16dbb5ed
Graeme Rocher <graeme.rocher@gmail.com>
2014-05-09 14:59:42 +0200

    update version to 1.1.6

--- CacheGrailsPlugin.groovy
+++ CacheGrailsPlugin.groovy
@@ -33,7 +33,7 @@ class CacheGrailsPlugin {
 
 	private final Logger log = LoggerFactory.getLogger('grails.plugin.cache.CacheGrailsPlugin')
 
-	def version = '1.1.6-SNAPSHOT'
+	def version = '1.1.6'
 	def grailsVersion = '2.0 > *'
 	def observe = ['controllers', 'services']
 	def loadAfter = ['controllers', 'services']
