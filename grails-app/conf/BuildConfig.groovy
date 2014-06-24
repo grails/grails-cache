1a99b614e1d1d41f2e2dc2f0c6555a48fe721070
graemerocher <l9qsHaN0lytSYzvAqvpIOY>
2014-06-24 14:05:58 +0200

    add javassist to declared dependencies

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -2,7 +2,7 @@ import grails.util.Environment
 
 if(System.getenv('TRAVIS_BRANCH')) {
     grails.project.repos.grailsCentral.username = System.getenv("GRAILS_CENTRAL_USERNAME")
-    grails.project.repos.grailsCentral.password = System.getenv("GRAILS_CENTRAL_PASSWORD")    
+    grails.project.repos.grailsCentral.password = System.getenv("GRAILS_CENTRAL_PASSWORD")
 }
 
 grails.project.work.dir = 'target'
@@ -22,27 +22,29 @@ grails.project.dependency.resolution = {
 	}
 
 	dependencies {
-        compile 'cglib:cglib-nodep:2.2.2'
+
+    compile "org.javassist:javassist:3.17.1-GA"
+
 		test 'org.codehaus.gpars:gpars:1.0.0', {
 			export = false
 		}
 		test 'org.codehaus.jsr166-mirror:jsr166y:1.7.0', {
 			export = false
 		}
-        test 'net.sourceforge.nekohtml:nekohtml:1.9.18', { export = false }
-        test 'net.sourceforge.htmlunit:htmlunit:2.12', { export = false }
-        test 'net.sourceforge.htmlunit:htmlunit-core-js:2.12', { export = false }
+    test 'net.sourceforge.nekohtml:nekohtml:1.9.18', { export = false }
+    test 'net.sourceforge.htmlunit:htmlunit:2.12', { export = false }
+    test 'net.sourceforge.htmlunit:htmlunit-core-js:2.12', { export = false }
 	}
 
 	plugins {
 		build(":tomcat:7.0.52.1") {
 			export = false
 		}
-		
+
         runtime(":hibernate:3.6.10.13") {
 			export = false
 		}
-		
+
         if (Environment.current != Environment.TEST) {
 			build(':release:3.0.1', ':rest-client-builder:1.0.3') {
 				export = false
@@ -50,11 +52,11 @@ grails.project.dependency.resolution = {
 		}
 
         compile ':scaffolding:2.0.1', { export = false }
-		
+
         test(':functional-test:2.0.0') {
 			export = false
 		}
-		
+
         compile ':webxml:1.4.1'
 	}
 }
