f62138c832dff65a0d315c99e39fe87b07df14d0
Jeff Scott Brown <brownj@ociweb.com>
2016-07-20 10:29:52 -0500

    remove tabs

--- grails-app/services/com/demo/BasicCachingService.groovy
+++ grails-app/services/com/demo/BasicCachingService.groovy
@@ -9,61 +9,61 @@ import groovy.transform.CompileStatic
 @CompileStatic
 class BasicCachingService {
 
-	private int invocationCounter = 0
+    private int invocationCounter = 0
 
-	def getInvocationCounter() {
-		invocationCounter
-	}
+    def getInvocationCounter() {
+        invocationCounter
+    }
 
-	// end::get_data[]
+    // end::get_data[]
 
-	private int invocationCounter2 = 100
+    private int invocationCounter2 = 100
 
-	// tag::get_data[]
-	@Cacheable('basic')
-	def getData() {
-		++invocationCounter
-		'Hello World!'
-	}
-	// end::get_data[]
+    // tag::get_data[]
+    @Cacheable('basic')
+    def getData() {
+        ++invocationCounter
+        'Hello World!'
+    }
+    // end::get_data[]
 
-	@CacheEvict(value="basic", allEntries=true)
-	def resetData() {
-		invocationCounter = 0
-		invocationCounter2 = 100
-		'Reset'
-	}
+    @CacheEvict(value = "basic", allEntries = true)
+    def resetData() {
+        invocationCounter = 0
+        invocationCounter2 = 100
+        'Reset'
+    }
 
-	def getInvocationCounter2() {
-		invocationCounter2
-	}
+    def getInvocationCounter2() {
+        invocationCounter2
+    }
 
-	@Cacheable('basic')
-	def getData2() {
-		++invocationCounter2
-		'Hello World 2!'
-	}
+    @Cacheable('basic')
+    def getData2() {
+        ++invocationCounter2
+        'Hello World 2!'
+    }
 
-	@Cacheable('basic')
-	def getDataWithParams(String name) {
-		++invocationCounter
-		'Hello World!'
-	}
+    @Cacheable('basic')
+    def getDataWithParams(String name) {
+        ++invocationCounter
+        'Hello World!'
+    }
 
-	@Cacheable('basic')
-	def getData2WithParams(String name) {
-		++invocationCounter2
-		'Hello World 2!'
-	}
+    @Cacheable('basic')
+    def getData2WithParams(String name) {
+        ++invocationCounter2
+        'Hello World 2!'
+    }
 
-	@Cacheable(value='basic', key='#key')
-	def getData(String key) {
-	}
+    @Cacheable(value = 'basic', key = '#key')
+    def getData(String key) {
+    }
 
-	@CachePut(value='basic', key='#key')
-	def getData(String key, String value) {
-		"** ${value} **"
-	}
-	// tag::get_data[]
+    @CachePut(value = 'basic', key = '#key')
+    def getData(String key, String value) {
+        "** ${value} **"
+    }
+    // tag::get_data[]
 }
 // end::get_data[]
