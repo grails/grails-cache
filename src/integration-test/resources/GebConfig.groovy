ba8adc344431a0c32b7b785cb1a4288a917c2437
eusorov <evgeny.usorov@secretescapes.com>
2024-01-24 01:58:34 +0100

    update to grails 5.3.5, use webDriverManagerVersion for automatically download and manage webdrivers for functional tests

--- src/integration-test/resources/GebConfig.groovy
+++ src/integration-test/resources/GebConfig.groovy
@@ -1,3 +1,6 @@
+import io.github.bonigarcia.wdm.WebDriverManager
+import io.github.bonigarcia.wdm.managers.ChromeDriverManager
+import io.github.bonigarcia.wdm.managers.FirefoxDriverManager
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
 import org.openqa.selenium.firefox.FirefoxDriver
@@ -8,16 +11,19 @@ environments {
 
     // You need to configure in Safari -> Develop -> Allowed Remote Automation
     safari {
+        WebDriverManager.safaridriver().setup();
         driver = { new SafariDriver() }
     }
 
     // run via “./gradlew -Dgeb.env=chrome iT”
     chrome {
+        WebDriverManager.chromedriver().setup();
         driver = { new ChromeDriver() }
     }
 
     // run via “./gradlew -Dgeb.env=chromeHeadless iT”
     chromeHeadless {
+        WebDriverManager.chromedriver().setup();
         driver = {
             ChromeOptions o = new ChromeOptions()
             o.addArguments('headless')
@@ -27,6 +33,7 @@ environments {
 
     // run via “./gradlew -Dgeb.env=firefoxHeadless iT”
     firefoxHeadless {
+        WebDriverManager.firefoxdriver().setup();
         driver = {
             FirefoxOptions o = new FirefoxOptions()
             o.addArguments('-headless')
@@ -36,6 +43,7 @@ environments {
 
     // run via “./gradlew -Dgeb.env=firefox iT”
     firefox {
+        WebDriverManager.firefoxdriver().setup();
         driver = { new FirefoxDriver() }
     }
 }
